afdccc4d29417cc040ad9c45de84d052f4566aba
Justin Trusty <justintrusty@gmail.com>
2024-10-11 14:41:16 -0500

    fixes with latest for it-tools & linux-tools

---
+++ apps/linux-tools/ci/latest.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+version=$(curl -sX GET "https://registry.hub.docker.com/v2/repositories/library/alpine/tags" | jq -r '.results[].name' | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n 1)
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
+
+
