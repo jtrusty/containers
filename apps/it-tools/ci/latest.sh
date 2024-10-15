c5ada25be4add7ae33214c919301ac42e92310c1
Justin Trusty <justintrusty@gmail.com>
2024-10-15 09:24:38 -0500

    updated latest to ignore errors

--- apps/it-tools/ci/latest.sh
+++ apps/it-tools/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://github.com/CorentinTh/it-tools/releases" | grep -oP '(v(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)-(?<revision>.*)).*(?=</h2>)' | sort -V | tail -n 1)
+version=$(curl -sX GET "https://github.com/CorentinTh/it-tools/releases" | grep -oP '(v(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)-(?<revision>.*)).*(?=</h2>)' | sort -V | tail -n 1 2>/dev/null)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
--- apps/linux-tools/ci/latest.sh
+++ apps/linux-tools/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://registry.hub.docker.com/v2/repositories/library/alpine/tags" | jq -r '.results[].name' | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n 1)
+version=$(curl -sX GET "https://registry.hub.docker.com/v2/repositories/library/alpine/tags" | jq -r '.results[].name' | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n 1 2>/dev/null)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
