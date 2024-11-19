fa17e448de27912040ecd5289127ff2228dc5b82
Justin Trusty <justintrusty@gmail.com>
2024-11-19 09:15:32 -0600

    updating entrypoint for mylar3

--- apps/mylar3/entrypoint.sh
+++ apps/mylar3/entrypoint.sh
@@ -2,7 +2,7 @@
 #shellcheck disable=SC2086
 
 exec \
-    python3 /app/mylar/Mylar.py \
+    python3 /app/mylar3/Mylar.py \
         --nolaunch \
         --quiet \
         --datadir=/config/mylar \
